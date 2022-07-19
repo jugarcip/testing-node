21bbd2868581a0df1f9d58644806cc30cc777036
Juan García Pérez <tyris.juan.garcia@gmail.com>
2022-07-19 14:10:28 +0200

    chore: wip

--- app.js
+++ app.js
@@ -1,91 +1,91 @@
 'use strict';
 
 const fs = require('fs');
-const name = "GALAXY_RAPTOR";
+const name = "NIGHT_EYE";
 
 let rawdata = fs.readFileSync("./assets/" + name + ".json");
 let metadata = JSON.parse(rawdata);
 metadata.forEach(element => {
     element.attributes.forEach(attribute => {
         if(attribute.trait_type === 'number' && attribute.value === '1') {
-            element.attributes.push({ trait_type: "colour", value: "black"});
-            element.attributes.push({ trait_type: "background", value: "blackhole"});
+            element.attributes.push({ trait_type: "colour", value: "beige"});
+            element.attributes.push({ trait_type: "background", value: "gas giant"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '2') {
             element.attributes.push({ trait_type: "colour", value: "blue"});
-            element.attributes.push({ trait_type: "background", value: "planet desert ring"});
+            element.attributes.push({ trait_type: "background", value: "star system"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '3') {
-            element.attributes.push({ trait_type: "colour", value: "brown"});
-            element.attributes.push({ trait_type: "background", value: "forming planet"});
+            element.attributes.push({ trait_type: "colour", value: "green"});
+            element.attributes.push({ trait_type: "background", value: "blackhole"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '4') {
-            element.attributes.push({ trait_type: "colour", value: "green"});
-            element.attributes.push({ trait_type: "background", value: "gas giant"});
+            element.attributes.push({ trait_type: "colour", value: "orange"});
+            element.attributes.push({ trait_type: "background", value: "planet desert ring"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '5') {
-            element.attributes.push({ trait_type: "colour", value: "orange"});
-            element.attributes.push({ trait_type: "background", value: "star system"});
+            element.attributes.push({ trait_type: "colour", value: "red"});
+            element.attributes.push({ trait_type: "background", value: "forming planet"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '6') {
-            element.attributes.push({ trait_type: "colour", value: "red"});
-            element.attributes.push({ trait_type: "background", value: "blackhole"});
+            element.attributes.push({ trait_type: "colour", value: "silver"});
+            element.attributes.push({ trait_type: "background", value: "gas giant"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '7') {
-            element.attributes.push({ trait_type: "colour", value: "silver"});
-            element.attributes.push({ trait_type: "background", value: "planet desert ring"});
-
-        } else if(attribute.trait_type === 'number' && attribute.value === '8') {
-            element.attributes.push({ trait_type: "colour", value: "black"});
-            element.attributes.push({ trait_type: "background", value: "forming planet"});
-
-        } else if(attribute.trait_type === 'number' && attribute.value === '9') {
-            element.attributes.push({ trait_type: "colour", value: "brown"});
-            element.attributes.push({ trait_type: "background", value: "gas giant"});
-
-        } else if(attribute.trait_type === 'number' && attribute.value === '10') {
-            element.attributes.push({ trait_type: "colour", value: "orange"});
+            element.attributes.push({ trait_type: "colour", value: "tan"});
             element.attributes.push({ trait_type: "background", value: "star system"});
 
+        } else if(attribute.trait_type === 'number' && attribute.value === '8') {
+            element.attributes.push({ trait_type: "colour", value: "red"});
+            element.attributes.push({ trait_type: "background", value: "blackhole"});
+
+        } else if(attribute.trait_type === 'number' && attribute.value === '9') {
+            element.attributes.push({ trait_type: "colour", value: "beige"});
+            element.attributes.push({ trait_type: "background", value: "planet desert ring"});
+
+        } else if(attribute.trait_type === 'number' && attribute.value === '10') {
+            element.attributes.push({ trait_type: "colour", value: "green"});
+            element.attributes.push({ trait_type: "background", value: "forming planet"});
+
         } else if(attribute.trait_type === 'number' && attribute.value === '11') {
+            element.attributes.push({ trait_type: "colour", value: "tan"});
+            element.attributes.push({ trait_type: "background", value: "gas giant"});
+
+        } else if(attribute.trait_type === 'number' && attribute.value === '12') {
+            element.attributes.push({ trait_type: "colour", value: "red"});
+            element.attributes.push({ trait_type: "background", value: "star system"});
+
+        } else if(attribute.trait_type === 'number' && attribute.value === '13') {
             element.attributes.push({ trait_type: "colour", value: "silver"});
             element.attributes.push({ trait_type: "background", value: "blackhole"});
 
-        } else if(attribute.trait_type === 'number' && attribute.value === '12') {
+        } else if(attribute.trait_type === 'number' && attribute.value === '14') {
             element.attributes.push({ trait_type: "colour", value: "blue"});
             element.attributes.push({ trait_type: "background", value: "planet desert ring"});
-
-        } else if(attribute.trait_type === 'number' && attribute.value === '13') {
-            element.attributes.push({ trait_type: "colour", value: "red"});
-            element.attributes.push({ trait_type: "background", value: "forming planet"});
-
-        } else if(attribute.trait_type === 'number' && attribute.value === '14') {
-            element.attributes.push({ trait_type: "colour", value: "green"});
-            element.attributes.push({ trait_type: "background", value: "gas giant"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '15') {
             element.attributes.push({ trait_type: "colour", value: "orange"});
-            element.attributes.push({ trait_type: "background", value: "star system"});
+            element.attributes.push({ trait_type: "background", value: "forming planet"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '16') {
-            element.attributes.push({ trait_type: "colour", value: "black"});
-            element.attributes.push({ trait_type: "background", value: "blackhole"});
+            element.attributes.push({ trait_type: "colour", value: "silver"});
+            element.attributes.push({ trait_type: "background", value: "gas giant"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '17') {
-            element.attributes.push({ trait_type: "colour", value: "red"});
-            element.attributes.push({ trait_type: "background", value: "planet desert ring"});
+            element.attributes.push({ trait_type: "colour", value: "beige"});
+            element.attributes.push({ trait_type: "background", value: "star system"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '18') {
             element.attributes.push({ trait_type: "colour", value: "blue"});
-            element.attributes.push({ trait_type: "background", value: "forming planet"});
+            element.attributes.push({ trait_type: "background", value: "blackhole"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '19') {
             element.attributes.push({ trait_type: "colour", value: "green"});
-            element.attributes.push({ trait_type: "background", value: "gas giant"});
+            element.attributes.push({ trait_type: "background", value: "planet desert ring"});
 
         } else if(attribute.trait_type === 'number' && attribute.value === '20') {
-            element.attributes.push({ trait_type: "colour", value: "silver"});
-            element.attributes.push({ trait_type: "background", value: "star system"});
+            element.attributes.push({ trait_type: "colour", value: "orange"});
+            element.attributes.push({ trait_type: "background", value: "forming planet"});
         }
     })
 });
